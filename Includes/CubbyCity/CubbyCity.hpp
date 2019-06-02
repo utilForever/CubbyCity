1f39e520ef98f6ffcc74f70604ae9948ce868add
Chris Ohk <utilForever@gmail.com>
2019-06-03 00:16:44 +0900

    feat(parse_data): Create CubbyCity class

---
+++ Includes/CubbyCity/CubbyCity.hpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2019 Chris Ohk, Paul Kweon, Den So, Edward Sung
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYCITY_HPP
+#define CUBBYCITY_HPP
+
+namespace CubbyCity
+{
+class CubbyCity
+{
+ public:
+    void Process();
+};
+}  // namespace CubbyCity
+
+#endif  // CUBBYCITY_HPP
---
+++ Sources/cubbycity/CubbyCity.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2019 Chris Ohk, Paul Kweon, Den So, Edward Sung
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <CubbyCity/CubbyCity.hpp>
+
+namespace CubbyCity
+{
+void CubbyCity::Process()
+{
+    // Parse tile
+
+    // Download data
+}
+}  // namespace CubbyCity
