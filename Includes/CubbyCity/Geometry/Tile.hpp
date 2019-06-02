73d42bf47203820f1814fdb0befd74e852a03090
Chris Ohk <utilForever@gmail.com>
2019-06-02 23:25:50 +0900

    feat(parse_data): Create Tile struct

---
+++ Includes/CubbyCity/Geometry/Tile.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2019 Chris Ohk, Paul Kweon, Den So, Edward Sung
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYCITY_TILE_HPP
+#define CUBBYCITY_TILE_HPP
+
+namespace CubbyCity
+{
+struct Tile
+{
+    int x;
+    int y;
+    int z;
+};
+}  // namespace CubbyCity
+
+#endif  // CUBBYCITY_TILE_HPP
