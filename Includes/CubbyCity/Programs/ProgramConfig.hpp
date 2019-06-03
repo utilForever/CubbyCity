855737a717d07bac0b98c0d62b8ca0307c443425
Chris Ohk <utilForever@gmail.com>
2019-06-03 20:59:30 +0900

    feat(parse_data): Create ProgramConfig struct

---
+++ Includes/CubbyCity/Programs/ProgramConfig.hpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2019 Chris Ohk, Paul Kweon, Den So, Edward Sung
+
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYCITY_PROGRAM_CONFIG_HPP
+#define CUBBYCITY_PROGRAM_CONFIG_HPP
+
+#include <string>
+
+namespace CubbyCity
+{
+struct ProgramConfig
+{
+    std::string apiKey;
+
+    std::string tileX;
+    std::string tileY;
+    int tileZ;
+};
+}  // namespace CubbyCity
+
+#endif  // CUBBYCITY_PROGRAM_CONFIG_HPP
