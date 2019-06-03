eda72ea99bb6b5dbb3985d8fbaf7cfb4f81ac31d
Chris Ohk <utilForever@gmail.com>
2019-06-04 01:42:55 +0900

    feat(parse_data): Add constructor and some variables
    - borders, invScale, tileOrigin

--- Includes/CubbyCity/Geometry/Tile.hpp
+++ Includes/CubbyCity/Geometry/Tile.hpp
@@ -7,13 +7,34 @@
 #ifndef CUBBYCITY_TILE_HPP
 #define CUBBYCITY_TILE_HPP
 
+#include <CubbyCity/Geometry/GeometryUtils.hpp>
+
+#include <glm/glm.hpp>
+
+#include <bitset>
+
 namespace CubbyCity
 {
 struct Tile
 {
+    Tile(int _x, int _y, int _z) : x(_x), y(_y), z(_z)
+    {
+        glm::dvec4 bounds = GetTileBounds(x, y, z, 256.0);
+        tileOrigin = glm::dvec2(0.5 * (bounds.x + bounds.z),
+                                -0.5 * (bounds.y + bounds.w));
+        double scale = 0.5 * glm::abs(bounds.x - bounds.z);
+        invScale = 1.0 / scale;
+        borders = 0;
+    }
+
     int x;
     int y;
     int z;
+
+    std::bitset<4> borders;
+
+    double invScale = 0.0;
+    glm::dvec2 tileOrigin;
 };
 }  // namespace CubbyCity
 
