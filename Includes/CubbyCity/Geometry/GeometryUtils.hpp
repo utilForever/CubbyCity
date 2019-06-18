a7f54019c5ebf89b40b2f3ff2c1032abbedbd652
Chris Ohk <utilForever@gmail.com>
2019-06-18 21:04:02 +0900

    feat(build-mesh): Add ComputeNormals() function

--- Includes/CubbyCity/Geometry/GeometryUtils.hpp
+++ Includes/CubbyCity/Geometry/GeometryUtils.hpp
@@ -27,7 +27,7 @@ inline glm::dvec2 ConvertPixelToMeters(const glm::dvec2 pixel, const int zoom,
                                        double invTileSize)
 {
     glm::dvec2 meters;
-    double res = (2.0 * HALF_CIRCUMFERENCE * invTileSize) / (1 << zoom);
+    const double res = (2.0 * HALF_CIRCUMFERENCE * invTileSize) / (1 << zoom);
     meters.x = pixel.x * res - HALF_CIRCUMFERENCE;
     meters.y = pixel.y * res - HALF_CIRCUMFERENCE;
 
@@ -45,6 +45,31 @@ inline glm::dvec4 GetTileBounds(int x, int y, int z, double tileSize)
 inline bool IsWithinTileRange(const glm::vec2& pos)
 {
     return pos.x >= -1.0 && pos.x <= 1.0 && pos.y >= -1.0 && pos.y <= 1.0;
+}
+
+inline void ComputeNormals(PolygonMesh& mesh)
+{
+    for (size_t i = 0; i < mesh.indices.size(); i += 3)
+    {
+        const int i1 = mesh.indices[i + 0];
+        const int i2 = mesh.indices[i + 1];
+        const int i3 = mesh.indices[i + 2];
+
+        const glm::vec3& v1 = mesh.vertices[i1].position;
+        const glm::vec3& v2 = mesh.vertices[i2].position;
+        const glm::vec3& v3 = mesh.vertices[i3].position;
+
+        const glm::vec3 d = glm::normalize(glm::cross(v2 - v1, v3 - v1));
+
+        mesh.vertices[i1].normal += d;
+        mesh.vertices[i2].normal += d;
+        mesh.vertices[i3].normal += d;
+    }
+
+    for (auto& v : mesh.vertices)
+    {
+        v.normal = glm::normalize(v.normal);
+    }
 }
 }  // namespace CubbyCity
 
